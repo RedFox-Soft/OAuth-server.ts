8e2e3998e4263d28f84e154c75441e64cba8c011
Anton Shchekota <anton.redfox@gmail.com>
2026-02-10 17:39:42 +0300

    Fix user info Bearer token unit tets

--- test/userinfo/bearer.spec.ts
+++ test/userinfo/bearer.spec.ts
@@ -1,93 +1,54 @@
-import bootstrap from '../test_helper.js';
+import { describe, it, beforeAll, expect } from 'bun:test';
+import bootstrap, { agent } from '../test_helper.js';
 
 describe('providing Bearer token', () => {
-	before(bootstrap(import.meta.url));
-	context('invalid requests', () => {
-		it('nothing provided', function () {
-			return this.agent
-				.get('/me')
-				.expect(
-					this.failWith(400, 'invalid_request', 'no access token provided')
-				);
+	beforeAll(async () => {
+		await bootstrap(import.meta.url)();
+	});
+
+	describe('invalid requests', () => {
+		it('nothing provided', async function () {
+			const { error } = await agent.userinfo.get();
+			if (!error) {
+				throw new Error('Have to be exception');
+			}
+			expect(error.status).toBe(422);
+			expect(error.value).toEqual({
+				error: 'invalid_request',
+				error_description: 'no access token provided'
+			});
 		});
 
-		it('provided twice', function () {
-			return this.agent
-				.get('/me')
-				.auth('auth', 'provided')
-				.query({ access_token: 'whaaat' })
-				.expect(
-					this.failWith(
-						400,
-						'invalid_request',
-						'access token must only be provided using one mechanism'
-					)
-				);
+		it('bad Authorization header format (one part)', async function () {
+			const { error } = await agent.userinfo.get({
+				headers: {
+					authorization: 'Bearer'
+				}
+			});
+			if (!error) {
+				throw new Error('Have to be exception');
+			}
+			expect(error.status).toBe(401);
+			expect(error.value).toEqual({
+				error: 'invalid_header_authorization',
+				error_description: 'invalid authorization header value format'
+			});
 		});
 
-		it('bad Authorization header format (one part)', function () {
-			return this.agent
-				.get('/me')
-				.set('Authorization', 'Bearer')
-				.expect(
-					this.failWith(
-						400,
-						'invalid_request',
-						'invalid authorization header value format'
-					)
-				);
-		});
-
-		it('bad Authorization header format (more then two parts)', function () {
-			return this.agent
-				.get('/me')
-				.auth('some three', { type: 'bearer' })
-				.expect(
-					this.failWith(
-						400,
-						'invalid_request',
-						'invalid authorization header value format'
-					)
-				);
-		});
-
-		it('bad Authorization header format (not bearer)', function () {
-			return this.agent
-				.get('/me')
-				.set('Authorization', 'Basic some')
-				.expect(
-					this.failWith(
-						400,
-						'invalid_request',
-						'authorization header scheme must be `Bearer`'
-					)
-				);
-		});
-
-		it('[query] empty token provided', function () {
-			return this.agent
-				.get('/me')
-				.query({ access_token: '' })
-				.expect(
-					this.failWith(400, 'invalid_request', 'no access token provided')
-				);
-		});
-
-		it('[body] empty token provided', function () {
-			return this.agent
-				.post('/me')
-				.send('access_token=')
-				.expect(
-					this.failWith(400, 'invalid_request', 'no access token provided')
-				);
-		});
-
-		it('empty body w/ auth header', function () {
-			return this.agent
-				.post('/me')
-				.expect(
-					this.failWith(400, 'invalid_request', 'no access token provided')
-				);
+		it('bad Authorization header format (not bearer)', async function () {
+			const { error } = await agent.userinfo.get({
+				headers: {
+					authorization: 'Basic some'
+				}
+			});
+			if (!error) {
+				throw new Error('Have to be exception');
+			}
+			expect(error.status).toBe(401);
+			expect(error.value).toEqual({
+				error: 'invalid_header_authorization',
+				error_description: 'authorization header scheme must be `Bearer`'
+			});
 		});
 	});
 });
